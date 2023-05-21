ecfce42988b18267fe5b44a4da60b9f36213c91f
nakazi <91645661+nakazi0635@users.noreply.github.com>
2023-05-21 11:59:55 +0900

    カーテンのような動きと、カーテンの中心座用を同期させるスプリクトファイルと生成

---
+++ Assets/Scripts/Skinned.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skinned : MonoBehaviour
+{
+    public GameObject skinnedMeshObject;
+    public GameObject targetObject;
+    public float offsetDistance = 0.1f; // 手前に配置する距離
+
+    private SkinnedMeshRenderer skinnedMeshRenderer;
+    private Transform targetTransform;
+
+    private void Start()
+    {
+        skinnedMeshRenderer = skinnedMeshObject.GetComponent<SkinnedMeshRenderer>();
+        targetTransform = targetObject.transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 center = skinnedMeshRenderer.bounds.center;
+        Vector3 offset = skinnedMeshRenderer.transform.up * offsetDistance; // 手前に移動するオフセットベクトル
+        targetTransform.position = center + offset;
+    }
+}
